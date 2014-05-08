40cb594f10ab366248cfe2245a6293a8ea6110e7
evshary <evshary@gmail.com>
2014-05-08 09:42:07 +0800

    Parse the data from ptt

--- parser.py
+++ parser.py
@@ -1,23 +1,127 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from HTMLParser import HTMLParser
 import urllib2
+import pdb
 
-class MyHTMLParser(HTMLParser):
+BaseURL = "http://www.ptt.cc"
+
+class MyBoardParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.links = []
+		self.nextPage = ""
+		self.link_flag = 0
+
 	def handle_starttag(self, tag, attrs):
-		print "Start Tag:", tag
-	def handle_endtag(self, tag):
-		print "End Tag:", tag
+		if tag == "div" and ("class","title") in attrs:
+			self.link_flag = 1
+		elif tag == "a" and self.link_flag == 1:
+			for (variable, value) in attrs:
+				if variable == "href":
+					self.links.append(value)
+			self.link_flag = 0
+		elif tag == "a" and self.link_flag == 2:
+			for (variable, value) in attrs:
+				if variable == "href":
+					self.nextPage = value
+			self.link_flag = 0
+			
 	def handle_data(self, data):
-		print "Data:", data
+		if data == u"最舊":
+			self.link_flag = 2
+
+class MyArticleParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.users = {"authors":{},"pushers":{}}
+		self.name_flag = 0
+		self.year_flag = 0
+
+	def handle_starttag(self, tag, attrs):
+		if tag == "span" and ("class","f3 hl push-userid") in attrs:
+			self.name_flag = 1
+		#elif tag == "span" and ("class","article-meta-value") in attrs:
+		#	self.name_flag = 2
+
+	#def handle_endtag(self, tag):
+	#	print "End Tag:", tag
+
+	def handle_data(self, data):
+		if self.name_flag == 1:
+			try:
+				self.users["pushers"][data] += 1
+			except:
+				self.users["pushers"][data] = 1
+			self.name_flag = 0
+		elif self.name_flag == 2:
+			data = data.split(" ")[0]
+			try:
+				self.users["authors"][data] += 1
+			except:
+				self.users["authors"][data] = 1
+			self.name_flag = 0
+		if self.year_flag == 1:
+			if "2014" not in data:
+				self.year_flag = -1
+			else:
+				self.year_flag = 0
+
+		if data == u"作者":
+			self.name_flag = 2
+		elif data == u"時間":
+			self.year_flag = 1
 
 def main():
-	req = urllib2.Request("http://www.google.com")
-	response = urllib2.urlopen(req)
-	htmlSrc = response.read()
-	parser = MyHTMLParser()
-	parser.feed(htmlSrc)
-	parser.close()
+	global BaseURL
+	file_handle = open("board.txt", "r")
+	for board in file_handle:
+		users_name = {}
+		page = "/bbs/"+board.strip()+"/index.html"
+		ArticleParser = MyArticleParser()
+		while True:
+			links = []
+			#Parse Index
+			bbsURL = BaseURL+page
+			print bbsURL
+			while True:
+				try:
+					htmlSrc = urllib2.urlopen(urllib2.Request(bbsURL)).read()
+					break
+				except:
+					continue
+			htmlSrc = htmlSrc.decode('utf-8')
+			BoardParser = MyBoardParser()
+			BoardParser.feed(htmlSrc)
+			BoardParser.close()
+			links =  BoardParser.links
+			page = BoardParser.nextPage
+			#Parse Article	
+			for article in links:
+				try_count = 0
+				while True:
+					try:
+						bbsURL = BaseURL+article
+						#print bbsURL
+						htmlSrc = urllib2.urlopen(urllib2.Request(bbsURL)).read()
+						htmlSrc = htmlSrc.decode('utf-8')
+						ArticleParser.feed(htmlSrc)
+						break
+					except:
+						if try_count > 5:
+							break
+						else:
+							try_count += 1
+						continue
+				if ArticleParser.year_flag == -1:
+					break
+			users_name = ArticleParser.users
+			print users_name
+			if ArticleParser.year_flag == -1:
+				break
+		ArticleParser.close()
+	file_handle.close()
 
 if __name__ == '__main__':
 	main()
